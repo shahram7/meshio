cd0a9fc1548cbeccb4faa01e22a7c3d4f9c32a91
Nils Meyer <nils.meyer@kit.edu>
2019-10-25 18:13:22 +0200

    Added plugin to build tensor fields.

---
+++ abq_meshio/combine.py
@@ -0,0 +1,89 @@
+"""Combine several state variables to new variables in Abaqus."""
+from abaqus import session
+from abaqusConstants import (MAX_PRINCIPAL, MID_PRINCIPAL, MIN_PRINCIPAL,
+                             SINGLE_PRECISION, TENSOR_3D_FULL,
+                             INTEGRATION_POINT)
+
+
+def tensor(odb_name, field_name, desc, s1, s2, s3, s4, s5, s6):
+    """Build a tensor.
+
+    Parameters
+    ----------
+    odb_name : str
+        Name of output databse.
+    field_name : str
+        Name of field to be generated.
+    desc : str
+        Description of field to be generated.
+    s1 : str
+        Name of 11 component.
+    s2 : str
+        Name of 22 component.
+    s3 : str
+        Name of 33 component.
+    s4 : str
+        Name of 12 component.
+    s5 : str
+        Name of 23 component.
+    s6 : str
+        Name of 13 component.
+
+    """
+    # close ODB and open with write permissions
+    odb = session.openOdb(name=odb_name)
+    odb.close()
+    odb = session.openOdb(name=odb_name, readOnly=False)
+    # define invariants to be computed
+    invariants = (MAX_PRINCIPAL, MID_PRINCIPAL, MIN_PRINCIPAL)
+
+    # for each step
+    for stepName in odb.steps.keys():
+        # for each frame
+        for i, frame in enumerate(odb.steps[stepName].frames):
+            sdv1 = frame.fieldOutputs[s1]
+            sdv2 = frame.fieldOutputs[s2]
+            sdv3 = frame.fieldOutputs[s3]
+            sdv4 = frame.fieldOutputs[s4]
+            sdv5 = frame.fieldOutputs[s6]  # different order in ODB (WTF ABQ?!)
+            sdv6 = frame.fieldOutputs[s5]  # different order in ODB (WTF ABQ?!)
+            labels = []
+            data = []
+            for s1c, s2c, s3c, s4c, s5c, s6c in zip(sdv1.values, sdv2.values,
+                                                    sdv3.values, sdv4.values,
+                                                    sdv5.values, sdv6.values):
+                if s1c.precision == SINGLE_PRECISION:
+                    data.append((s1c.data, s2c.data, s3c.data,
+                                 s4c.data, s5c.data, s6c.data))
+                else:
+                    data.append((s1c.dataDouble, s2c.dataDouble,
+                                 s3c.dataDouble, s4c.dataDouble,
+                                 s5c.dataDouble, s6c.dataDouble))
+                labels.append(s1c.elementLabel)
+
+            # create empty field output
+            Field = frame.FieldOutput(
+                name=field_name, description=desc,
+                type=TENSOR_3D_FULL,
+                validInvariants=invariants)
+            # fill field output with values
+            instance = sdv1.values[0].instance
+            if (instance.name == sdv2.values[0].instance.name and
+                    instance.name == sdv3.values[0].instance.name and
+                    instance.name == sdv4.values[0].instance.name and
+                    instance.name == sdv5.values[0].instance.name and
+                    instance.name == sdv6.values[0].instance.name):
+                Field.addData(
+                    position=INTEGRATION_POINT, instance=instance,
+                    labels=labels, data=data)
+            else:
+                print("Could not create field, data is from different"
+                      " instances.")
+
+    odb.save()
+    odb.close()
+    odb = session.openOdb(name=odb_name)
+    current_viewport = session.currentViewportName
+    session.viewports[current_viewport].setValues(displayedObject=odb)
+    print('Done.')
+    return 1
---
+++ meshio_plugin/meshioGui.py
@@ -0,0 +1,118 @@
+"""GUI for meshio and related tools in Postprocessing."""
+
+from abaqusGui import (AFXDataDialog, AFXComboBox, session, FXLabel, FXMAPFUNC,
+                       sendCommand, AFXTextField, DIALOG_ACTIONS_SEPARATOR,
+                       SEL_COMMAND, showAFXWarningDialog, AFXDialog, AFXForm)
+
+
+class CreateSymmTensor(AFXDataDialog):
+    """Create a new tensor field from given variables."""
+
+    [
+        ID_WARNING,
+    ] = range(AFXForm.ID_LAST, AFXForm.ID_LAST+1)
+
+    def __init__(self, form):
+        """Set up the initial dialog and connect Buttons to actions."""
+        self.form = form
+        # find current viewport
+        currentViewport = session.viewports[session.currentViewportName]
+        # assign odb file from current viewport
+        self.odbFile = currentViewport.displayedObject
+        # get file name and path
+        self.odbFileNameFull = self.odbFile.path
+
+        AFXDataDialog.__init__(self,
+                               form,
+                               'Create Tensor Field',
+                               0,
+                               DIALOG_ACTIONS_SEPARATOR)
+
+        FXLabel(self, 'This plugin creates a tensor from state variables.')
+
+        nvis = 0
+
+        self.nameTextField = AFXTextField(self, 35, 'Field name:')
+        self.descTextField = AFXTextField(self, 35, 'Description:')
+
+        variables = set()
+        step_name = self.odbFile.steps.keys()[0]
+        frame = self.odbFile.steps[step_name].frames[0]
+        variables.update(frame.fieldOutputs.keys())
+        variables = list(variables)
+
+        nvis = len(variables)
+        self.s1combo = AFXComboBox(self, 0, nvis, 'Component 11:')
+        self.s2combo = AFXComboBox(self, 0, nvis, 'Component 22:')
+        self.s3combo = AFXComboBox(self, 0, nvis, 'Component 33:')
+        self.s4combo = AFXComboBox(self, 0, nvis, 'Component 12:')
+        self.s5combo = AFXComboBox(self, 0, nvis, 'Component 23:')
+        self.s6combo = AFXComboBox(self, 0, nvis, 'Component 31:')
+        for variable in sorted(variables):
+            self.s1combo.appendItem(variable)
+            self.s2combo.appendItem(variable)
+            self.s3combo.appendItem(variable)
+            self.s4combo.appendItem(variable)
+            self.s5combo.appendItem(variable)
+            self.s6combo.appendItem(variable)
+        self.s1combo.setMaxVisible(10)
+        self.s2combo.setMaxVisible(10)
+        self.s3combo.setMaxVisible(10)
+        self.s4combo.setMaxVisible(10)
+        self.s5combo.setMaxVisible(10)
+        self.s6combo.setMaxVisible(10)
+
+        self.appendActionButton('Create Field', self, self.ID_CLICKED_APPLY)
+        self.appendActionButton(self.DISMISS)
+
+        FXMAPFUNC(self, SEL_COMMAND, self.ID_CLICKED_APPLY,
+                  CreateSymmTensor.doCustomChecks)
+        FXMAPFUNC(self, SEL_COMMAND, self.ID_WARNING,
+                  CreateSymmTensor.onCmdWarning)
+
+    def doCustomChecks(self, sender, sel, ptr):
+        """Ask user, because this is a permanent modification of the ODB."""
+        showAFXWarningDialog(self,
+                             'This will re-load the ODB in write mode and'
+                             ' adds fields permantenly. Are you sure?',
+                             AFXDialog.YES | AFXDialog.NO,
+                             self, self.ID_WARNING)
+
+    def onCmdWarning(self, sender, sel, ptr):
+        """Send command to build tensor or abort."""
+        if sender.getPressedButtonId() == AFXDialog.ID_CLICKED_YES:
+                self.create_field()
+        elif sender.getPressedButtonId() == AFXDialog.ID_CLICKED_NO:
+                self.form.deactivate()
+
+    def create_field(self):
+        """Process inputs."""
+        field_name = self.nameTextField.getText()
+        description = self.descTextField.getText()
+
+        item = self.s1combo.getCurrentItem()
+        s1 = self.s1combo.getItemText(item)
+        item = self.s2combo.getCurrentItem()
+        s2 = self.s2combo.getItemText(item)
+        item = self.s3combo.getCurrentItem()
+        s3 = self.s3combo.getItemText(item)
+        item = self.s4combo.getCurrentItem()
+        s4 = self.s4combo.getItemText(item)
+        item = self.s5combo.getCurrentItem()
+        s5 = self.s5combo.getItemText(item)
+        item = self.s6combo.getCurrentItem()
+        s6 = self.s6combo.getItemText(item)
+
+        sendCommand("from abq_meshio.combine import *")
+        cmdstr = ("tensor('" + self.odbFileNameFull + "', '" +
+                  field_name + "', '" +
+                  description + "', '" +
+                  s1 + "', '" +
+                  s2 + "', '" +
+                  s3 + "', '" +
+                  s4 + "', '" +
+                  s5 + "', '" +
+                  s6 + "')")
+        sendCommand(cmdstr)
+        self.form.deactivate()
+        return 1
