602daec7e75b03ce3b85752b63c81dca78d211fb
Nils Meyer <nils.meyer@kit.edu>
2020-03-17 16:07:16 +0100

    Installer is ready for Abaqus 2020.

--- plugins/abaqus/install.bat
+++ plugins/abaqus/install.bat
@@ -2,26 +2,31 @@
 SETLOCAL enabledelayedexpansion
 
 :: Set SIMULIA base directory here:
-SET root=C:\SIMULIA\CAE
+SET root=C:\SIMULIA\CAE\plugins
 
 SET dir=%~dp0
-SET plugindir=win_b64\code\python2.7\lib\abaqus_plugins
-SET codedir=win_b64\code\bin
 
 SET Index=1
 FOR /d %%D IN (%root%\*) DO (
   SET "versions[!Index!]=%%D"
   SET /a Index+=1
 )
-set /a UBound=Index-2
+set /a UBound=Index-1
 for /l %%i in (1,1,%UBound%) do (
   ECHO "Installing Meshio-Plugin to !versions[%%i]!"
   SET str=!versions[%%i]!
   SET version=!str:~-4!
-  :: Copy
-  XCOPY %dir%meshio_plugin %root%\!version!\%plugindir%\MeshioPlugin\* /YQ
-  XCOPY %dir%\..\..\meshio %root%\!version!\%codedir%\meshio\* /YQS
 
-  XCOPY %dir%abq_meshio %root%\!version!\%codedir%\abq_meshio\* /YQ
+  IF !version! GEQ 2020 (
+    SET codedir=C:\SIMULIA\EstProducts\!version!\win_b64\code\bin
+  ) ELSE (
+    SET codedir=C:\SIMULIA\CAE\!version!\win_b64\code\bin
+  )
+
+  :: Copy
+  XCOPY %dir%meshio_plugin %root%\!version!\MeshioPlugin\* /YQ
+
+  XCOPY %dir%\..\..\meshio !codedir!\meshio\* /YQS
+  XCOPY %dir%abq_meshio !codedir!\abq_meshio\* /YQ
 )
 PAUSE
